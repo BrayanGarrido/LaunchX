76ce41c5e2720d18698314066c40e308f2eebaf6
Brayan <brayangarmar@outlook.com>
2022-02-05 19:34:52 -0600

    Segundo Commmit

--- Módulo 1/modulo_1.py
+++ Módulo 1/modulo_1.py
@@ -7,6 +7,6 @@ print('Today´s date is: ' + str(date.today()) )
 #Convertidor de unidades
 parsec = 11
 lightyears = 0
-#1 parsec es 3.26156 años luz.
+#1 parsec es 3.26156 años luz
 lightyears = parsec * 3.26156
 print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
